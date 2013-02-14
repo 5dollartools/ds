efe21853d7466abe8ae3ff3328dd2286bdd28a7b
rahul <rahul@linux.(none)>
2013-02-14 19:08:30 +0530

    Added functions cll_add_node_head, cll_add_node_tail, cll_print.
    cll_delete is in progress.

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : CircularLinkList.c
- Author      : Rahul Bedge
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 struct node
@@ -16,67 +6,130 @@ struct node
 	struct node *next;
 };
 
-int cll_add_node (struct node **ppNode, int data )
+int cll_add_node_head ( struct node **start_node, int data)
 {
-	struct node *pTempNode = malloc (sizeof (struct node));
-	if ( !pTempNode )
-		return -1;
-
-	if (*ppNode == NULL)
+	struct node *tmp = malloc (sizeof(struct node)); 
+	if (!tmp)
+		return -1; 
+	
+	tmp->data = data; 
+	if ( !*start_node)
 	{
-		*ppNode = pTempNode;
+		*start_node = tmp; 
+		tmp->next = *start_node; 
 	}
 	else
 	{
-		struct node *pIter = *ppNode;
-		struct node *pPrev = *ppNode;
+		struct node *iter = *start_node; 
+		while ( iter && iter->next!=*start_node)
+			iter = iter->next; 
 
-		while ( pIter && pIter->next!=*ppNode)
-		{
-			printf ("%d %d", pIter->data, pIter->next->data);
-			pPrev = pIter;
-			pIter = pIter->next;
-		}
-		pPrev->next = pTempNode;
+		tmp->next = *start_node; 
+		*start_node = tmp; 
+		iter->next = *start_node; 
 	}
-	pTempNode->next = *ppNode;
+	return 0; 
+}
+
+int cll_add_node_tail (struct node **start_node, int data )
+{
+	struct node *tmp = malloc (sizeof(struct node));
+	if ( !tmp)
+		return -1;
+		
+	tmp->data = data; 
+
+	if (!*start_node)
+	{
+		*start_node = tmp;
+	}
+	else
+	{
+		struct node *iter = *start_node;
+
+		while ( iter && iter->next!=*start_node)
+			iter = iter->next;
+
+		iter->next = tmp; 
+	}
+	tmp->next = *start_node;
 	return 0;
 }
 
-void cll_print(struct node *pStart)
+void cll_print(struct node *start_node)
 {
-	if ( !pStart)
+	if ( !start_node)
 		return;
 
-	printf ("test \n");
-	while ( pStart && pStart->next != pStart)
+	printf ("\n");
+
+	struct node *iter = start_node; 
+	do 
 	{
-		printf ( "%d\t", pStart->data);
-		pStart = pStart->next;
-	}
+		printf ("%d ", iter->data); 
+		iter = iter->next; 
+	}while (iter!=start_node); 
+
+	printf ("\n"); 
 }
 
-void cll_initialize(struct node **ppNode)
+int cll_delete ( struct node ** start_node, int key)
 {
-	if ( *ppNode)
+	if ( !*start_node)
+		return -1; 
+		
+	struct node *iter = *start_node; 
+	struct node *prev = NULL;
+	
+	do 
 	{
-		(*ppNode)->data = 0;
-		(*ppNode)->next = NULL;
+		if ( key == iter->data)
+		{
+			if ( !prev)
+			{
+				struct node *tmp_iter = iter;
 
-	}
+				while ( tmp_iter  && tmp_iter->next!=iter)
+					tmp_iter = tmp_iter->next;
+				
+				
+			}
+			else
+			{
+				prev->next = iter->next; 
+			}
+			free ( iter); 
+			break; 
+		}
+		
+		prev = iter; 
+		iter = iter->next; 
+		
+	}while (iter!=*start_node); 
+	
+		
+	return 0; 
 }
 
 int main(void)
 {
-	struct node* pStart = NULL;
+	struct node* start_node = NULL;
+	cll_add_node_head(&start_node, 10);
+	cll_add_node_head(&start_node, 20);
+	cll_add_node_head(&start_node, 30);
+	cll_add_node_tail(&start_node, 40);
+	
+	/*cll_add_node_tail(&start_node, 10);
+	cll_add_node_tail(&start_node, 20);
+	cll_add_node_tail(&start_node, 30);
+	cll_add_node_tail(&start_node, 40);
+	cll_add_node_tail(&start_node, 50);*/
 
-	cll_initialize(&pStart);
-
-	cll_add_node(&pStart, 10);
-	cll_add_node(&pStart, 20);
-	cll_add_node(&pStart, 30);
-
-	cll_print(pStart);
-
+	cll_print(start_node);
+	
+	cll_delete(&start_node, 20); 
+	
+	cll_print(start_node);
+	
 	return EXIT_SUCCESS;
 }
